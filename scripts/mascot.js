24516d56d8e554615a9b61d298da8f2a9c368e9d
ukrioo <80760888+ukrioo@users.noreply.github.com>
2024-01-20 15:53:42 +0000

    hell?

--- scripts/mascot.js
+++ scripts/mascot.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('https://oldground.haydar.dev/api/get-waifu')
-        .then(response => response.json())
-        .then(data => {
-            const imageUrl = data.imageUrl;
+    const randomImageNumber = Math.floor(Math.random() * 69) + 1;
 
-            const imgElement = document.getElementById('mascot');
-            imgElement.src = imageUrl;
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
+    const imageUrl = `mascots/${randomImageNumber}.webp`;
+
+    const imgElement = document.getElementById('mascot');
+
+    imgElement.src = imageUrl;
+
+    imgElement.onload = function () {
+        console.log('Image loaded successfully!');
+    };
+
+    imgElement.onerror = function () {
+        console.error('Error loading image, setting fallback image.');
+        imgElement.src = 'styles/img/fallback-mascot.webp';
+    };
 });
